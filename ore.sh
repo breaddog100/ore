1dad0850886fd7786dc8d7cd4b3b086e8f606dc8
kimi <kimi@kimideMacBook-Air.local>
2024-08-05 20:41:08 +0800

    update

--- ore.sh
+++ ore.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
-set -e
+# 设置版本号
+current_version=20240805001
 
-# 脚本保存路径
-SCRIPT_PATH="$HOME/ore.sh"
+update_script() {
+    # 指定URL
+    update_url="https://raw.githubusercontent.com/breaddog100/ore/main/ore.sh"
+    file_name=$(basename "$update_url")
+
+    # 下载脚本文件
+    tmp=$(date +%s)
+    timeout 10s curl -s -o "$HOME/$tmp" -H "Cache-Control: no-cache" "$update_url?$tmp"
+    exit_code=$?
+    if [[ $exit_code -eq 124 ]]; then
+        echo "命令超时"
+        return 1
+    elif [[ $exit_code -ne 0 ]]; then
+        echo "下载失败"
+        return 1
+    fi
+
+    # 检查是否有新版本可用
+    latest_version=$(grep -oP 'current_version=([0-9]+)' $HOME/$tmp | sed -n 's/.*=//p')
+
+    if [[ "$latest_version" -gt "$current_version" ]]; then
+        clear
+        echo ""
+        # 提示需要更新脚本
+        printf "\033[31m脚本有新版本可用！当前版本：%s，最新版本：%s\033[0m\n" "$current_version" "$latest_version"
+        echo "正在更新..."
+        sleep 3
+        mv $HOME/$tmp $HOME/$file_name
+        chmod +x $HOME/$file_name
+        exec "$HOME/$file_name"
+    else
+        # 脚本是最新的
+        rm -f $tmp
+    fi
+
+}
 
 # 部署节点
 function install_node() {
@@ -193,6 +228,7 @@ function main_menu() {
 	while true; do
 	    clear
 	    echo "===============ORE一键部署脚本==============="
+		echo "当前版本：$current_version"
 	    echo "沟通电报群：https://t.me/lumaogogogo"
 	    echo "单号需要的资源：1C1G5G；CPU核心越多越好"
 		echo "请选择要执行的操作:"
@@ -219,6 +255,8 @@ function main_menu() {
         read -n 1
     done
 }
+# 检查更新
+update_script
 
 # 显示主菜单
 main_menu
